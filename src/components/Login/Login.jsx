02008cdb5360890440b166e26ecdcb6eefd000e5
innes-k <innes.kwak@gmail.com>
2024-02-08 00:10:25 +0900

    [css] styled-components 파일분리, 전반 비율 조정

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import styled from 'styled-components';
+import {
+  StCheckBox,
+  StContainer,
+  StIdPwWrapper,
+  StLoginBtn,
+  StLoginBtnWrapper,
+  StLoginInput,
+  StLoginTitle,
+  StLoginWrapper,
+  StLogoImg,
+  StLogoName,
+  StLogoWrapper,
+  StMailPw,
+  StMemoInfo,
+  StMemoWrapper,
+  StSignUp
+} from '../../styles/Login';
 
 const Login = () => {
   return (
     <StContainer>
       <StLogoWrapper>
-        <StLogoImg src='' alt='로고이미지' />
+        <StLogoImg src='test.png' alt='로고이미지' />
         <StLogoName>Travel Vibe</StLogoName>
       </StLogoWrapper>
       <StLoginWrapper>
-        <StTitle>로그인</StTitle>
+        <StLoginTitle>로그인</StLoginTitle>
         <StIdPwWrapper>
           <StMailPw>이메일</StMailPw>
           <StLoginInput />
@@ -28,150 +44,5 @@ const Login = () => {
     </StContainer>
   );
 };
-
-const StContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  box-sizing: border-box;
-  width: 1000px;
-  height: 800px;
-  border: 1px solid blue;
-`;
-
-const StLogoWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 400px;
-  height: 100%;
-`;
-
-const StLogoImg = styled.img`
-  width: 70px;
-  height: 70px;
-`;
-
-const StLogoName = styled.p`
-  font-family: 'Inter';
-  font-style: italic;
-  font-weight: 900;
-  font-size: 70px;
-  line-height: 85px;
-
-  color: #2673db;
-`;
-
-const StLoginWrapper = styled.div`
-  width: 600px;
-  height: 100%;
-`;
-
-const StTitle = styled.p`
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 55px;
-  line-height: 67px;
-
-  color: #2673db;
-`;
-
-const StIdPwWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const StMailPw = styled.p`
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 32px;
-  line-height: 39px;
-
-  color: #2673db;
-`;
-
-const StLoginInput = styled.input`
-  /* position: relative; */
-  width: 464.06px;
-  height: 59.08px;
-  left: 991px;
-  top: 534px;
-  border-radius: 3px;
-`;
-
-const StMemoWrapper = styled.div`
-  display: flex;
-`;
-
-const StCheckBox = styled.input`
-  box-sizing: border-box;
-
-  position: absolute;
-  left: 47.92%;
-  right: 50.8%;
-  top: 58.94%;
-  bottom: 38.96%;
-
-  background: #ffffff;
-  border: 3px solid #000000;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-`;
-
-const StMemoInfo = styled.p`
-  position: absolute;
-  left: 50.39%;
-  right: 38.68%;
-  top: 59.18%;
-  bottom: 39.24%;
-
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 24px;
-  line-height: 29px;
-
-  color: #000000;
-`;
-
-const StLoginBtnWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-
-  position: relative;
-  width: 101.89px;
-  height: 47.09px;
-  left: 990px;
-  top: 813px;
-`;
-
-const StLoginBtn = styled.button`
-  position: absolute;
-  left: 0%;
-  right: 0%;
-  top: 0%;
-  bottom: 0%;
-
-  background: #2673db;
-  border-radius: 5px;
-
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 24px;
-  line-height: 29px;
-
-  color: #ffffff;
-`;
-
-const StSignUp = styled.p`
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 24px;
-  line-height: 29px;
-  text-decoration-line: underline;
-
-  color: #000000;
-`;
 
 export default Login;
--- src/styles/Login.js
+++ src/styles/Login.js
@@ -1,1 +1,110 @@
-// 2673DB
+import styled from 'styled-components';
+
+export const StContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  box-sizing: border-box;
+  width: 700px;
+  height: 400px;
+  border: 1px solid #2673db;
+`;
+
+export const StLogoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 30px;
+`;
+
+export const StLogoImg = styled.img`
+  width: 70px;
+  height: 70px;
+`;
+
+export const StLogoName = styled.p`
+  font-weight: 900;
+  font-size: 40px;
+  line-height: 85px;
+
+  color: #2673db;
+`;
+
+export const StLoginWrapper = styled.div``;
+
+export const StLoginTitle = styled.p`
+  font-weight: 800;
+  font-size: 30px;
+  line-height: 67px;
+  text-align: center;
+
+  color: #2673db;
+`;
+
+export const StIdPwWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StMailPw = styled.p`
+  font-weight: 600;
+  font-size: 23px;
+  line-height: 39px;
+
+  color: #2673db;
+`;
+
+export const StLoginInput = styled.input`
+  width: 300px;
+  height: 40px;
+  left: 991px;
+  top: 534px;
+  border-radius: 5px;
+  border: 1px solid #2673db;
+  background-color: #e3e3e3;
+`;
+
+export const StMemoWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+export const StCheckBox = styled.input`
+  width: 20px;
+  margin: 20px;
+`;
+
+export const StMemoInfo = styled.p`
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 29px;
+
+  color: #000000;
+`;
+
+export const StLoginBtnWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const StLoginBtn = styled.button`
+  background: #2673db;
+  border-radius: 5px;
+  border: 0px;
+
+  font-weight: 600;
+  font-size: 17px;
+  height: 40px;
+
+  color: #ffffff;
+`;
+
+export const StSignUp = styled.p`
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 29px;
+  text-decoration-line: underline;
+
+  color: #000000;
+`;

a108b888e8893eda5b540b47f246e00ab1a733d4
picoloman6 <a2328b@naver.com>
2024-02-08 20:12:12 +0900

    [fix] Article import 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
-import Article from './pages/Article.jsx';
 import ResetStyles, { StAppWrapper } from './App.style.js';
 import Posting from './components/Posting/Posting.jsx';
 import MainPage from './pages/MainPage.jsx';
 import MyPage from './components/MyPage/MyPage.jsx';
 import ArticlePage from './pages/ArticlePage.jsx';
 import Modal from './components/Login/Modal.jsx';
-
 
 const App = () => {
   return (

be1d9f76cd346da7d2624187d39de4b8e9f9d84b
picoloman6 <a2328b@naver.com>
2024-02-08 20:00:00 +0900

    [fix] Login 확인

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import ResetStyles, { StAppWrapper } from './App.style.js';
 import MainPage from './pages/MainPage.jsx';
 import MyPage from './components/MyPage/MyPage.jsx';
 import ArticlePage from './pages/ArticlePage.jsx';
+import Modal from './components/Login/Modal.jsx';
 
 const App = () => {
   return (
     <StAppWrapper>
       <ResetStyles />
       <ArticlePage />
+      <Modal />
     </StAppWrapper>
   );
 };

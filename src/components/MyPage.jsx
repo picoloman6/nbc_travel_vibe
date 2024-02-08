06958a7d0bc675e381b3be8ac7353a757ba4d55c
seoyeon-1206 <ssy_1206@naver.com>
2024-02-08 13:52:34 +0900

    [Style] StMyPageTitle로 컴포넌트 이름 변경

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -5,7 +5,7 @@ import { StDeleteBtn, StSubmitBtn } from './Common';
 export const MyPage = () => {
     return (
         <StContainer>
-            <StTitle>프로필 설정</StTitle>
+            <StMyPageTitle>프로필 설정</StMyPageTitle>
             <StMyPageWrapper>
                 <StAvatar>
                 </StAvatar>
@@ -42,79 +42,79 @@ export const StContainer = styled.div`
     padding: 0px 300px
 `;
 
-export const StTitle = styled.h1`
-font-weight: 600;
-font-size: 48px;
-color: #2673DB;
+export const StMyPageTitle = styled.h1`
+    font-weight: 600;
+    font-size: 48px;
+    color: #2673DB;
 `
 export const StMyPageWrapper = styled.div`
-display: flex;
-flex-direction: row;
-border: 1px solid black;
-padding: 40px;
-gap: 60px;
+    display: flex;
+    flex-direction: row;
+    border: 1px solid black;
+    padding: 40px;
+    gap: 60px;
 `
 export const StAvatar = styled.div`
-width: 200px;
-height: 200px;
-background-color: green;
+    width: 200px;
+    height: 200px;
+    background-color: green;
 `
 
 export const StUserInfoWrapper = styled.div`
- display: flex;
- flex-direction: column;
- width: 500px; 
- gap: 40px;
- & label {
-    font-weight: 400;
-    font-size: 24px;
- }
- & input {
-    width: 100%; 
-    height: 48px;
-    background: #FFFFFF;
-    border: 1px solid #838383;
-    box-sizing: border-box; 
-    padding-left: 20px;
-    padding-left: 20px;
-    font-size: 24px; 
-  }
-& input ::placeholder {
-color: #838383;
-}
+    display: flex;
+    flex-direction: column;
+    width: 500px; 
+    gap: 40px;
+        & label {
+            font-weight: 400;
+            font-size: 24px;
+        }
+        & input {
+            width: 100%; 
+            height: 48px;
+            background: #FFFFFF;
+            border: 1px solid #838383;
+            box-sizing: border-box; 
+            padding-left: 20px;
+            padding-left: 20px;
+            font-size: 24px; 
+        }
+        & input ::placeholder {
+        color: #838383;
+        }
 `
 export const StEmail = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-gap: 20px;
- & div {
-width: 100%;
-height: 48px;
-box-sizing: border-box;
-background: #F2F2F2;
-font-weight: 600;
-font-size: 24px;
-color: #838383;
-display: flex;
-align-items: center; 
-padding-left: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 20px;
+        & div {
+        width: 100%;
+        height: 48px;
+        box-sizing: border-box;
+        background: #F2F2F2;
+        font-weight: 600;
+        font-size: 24px;
+        color: #838383;
+        display: flex;
+        align-items: center; 
+        padding-left: 20px;
  }
 `
 export const StNickName = styled.div`
     display: flex;
-flex-direction: column;
-gap: 20px;
+    flex-direction: column;
+    gap: 20px;
 `
 export const StCurrentPw = styled.div`
     display: flex;
-flex-direction: column;
-gap: 20px;
+    flex-direction: column;
+    gap: 20px;
 `
 export const StNewPw = styled.div`
     display: flex;
-flex-direction: column;
-gap: 20px;
+    flex-direction: column;
+    gap: 20px;
 `
 export const StBtnsWrapper = styled.div`
     display: flex;

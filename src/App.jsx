b68e23e5e8f0c160e30aa6a09a008d1ef8302658
picoloman6 <a2328b@naver.com>
2024-02-08 21:30:22 +0900

    [feature] 리듀서 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import ResetStyles, { StAppWrapper } from './App.style.js';
 import Posting from './components/Posting/Posting.jsx';
 import MainPage from './pages/MainPage.jsx';
@@ -6,7 +7,37 @@ import MyPage from './components/MyPage/MyPage.jsx';
 import ArticlePage from './pages/ArticlePage.jsx';
 import Modal from './components/Login/Modal.jsx';
 
+import { postUserData } from './redux/modules/UserReducer.js';
+import { postPostData } from './redux/modules/PostReducer.js';
+import { postCommentData } from './redux/modules/CommentReducer.js';
+import { useEffect } from 'react';
+
 const App = () => {
+  // 4. useSelector로 여기서 꺼내서 쓴다.
+  // state[전체 상태].user[리듀서이름].users[상태이름]
+  const users = useSelector((state) => state.user.users);
+  const posts = useSelector((state) => state.post.posts);
+  const comments = useSelector((state) => state.comment.comments);
+
+  // 5. 데이터에 변화를 줄때는 이대로 사용한다.
+  // dispatch 안에는 액션 객체 생성 함수
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(
+      postUserData({
+        userId: 3,
+        email: 'sdkfjl',
+        password: 'sdfsd',
+        nickname: 'sdfsdf',
+        image: 'dddd'
+      })
+    );
+  }, [dispatch]);
+
+  console.log(users);
+  console.log(posts);
+  console.log(comments);
+
   return (
     <StAppWrapper>
       <ResetStyles />

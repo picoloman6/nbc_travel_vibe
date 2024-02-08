d92b5fb40b46efca9bddade428476bd8e31c9d31
HuaHuaChiChi <ksksks102@naver.com>
2024-02-08 20:07:49 +0900

    [fix] 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import Article from './pages/Article.jsx';
-import ResetStyles from './App.style.js';
+import ResetStyles, { StAppWrapper } from './App.style.js';
 import Posting from './components/Posting/Posting.jsx';
-import MainPage from './pages/styles';
 
 const App = () => {
   return (

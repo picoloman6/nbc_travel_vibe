8a72767c99a753117df4015fb772405306cf33cd
seoyeon-1206 <ssy_1206@naver.com>
2024-02-08 13:50:49 +0900

    [Style] StUserInfoWrapper input, placeholder 폰트 변경

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -4,48 +4,43 @@ import { StDeleteBtn, StSubmitBtn } from './Common';
 
 export const MyPage = () => {
     return (
-        <>
-            <Container>
-                <StTitle>프로필 설정</StTitle>
-                <StMyPageWrapper>
-                    <StAvatar>
-                    </StAvatar>
-                    <StUserInfoWrapper>
-                        <StEmail>
-                            <label>이메일</label>
-                            <div>이메일 보여주는 박스</div>
-                        </StEmail>
-                        <StNickName>
-                            <label>닉네임</label>
-                            <input placeholder="닉네임" type='text'></input>
-                        </StNickName>
-                        <StCurrentPw>
-                            <label>현재 비밀번호</label>
-                            <input type='password'></input>
-                        </StCurrentPw>
-                        <StNewPw>
-                            <label>새로운 비밀번호</label>
-                            <input type='password'></input>
-                        </StNewPw>
-                    </StUserInfoWrapper>
-                </StMyPageWrapper>
-                <StBtnsWrapper>
-                    <StSubmitBtn>완료</StSubmitBtn>
-                    <StDeleteBtn>삭제</StDeleteBtn>
-                </StBtnsWrapper>
-            </Container>
-
-
-        </>
+        <StContainer>
+            <StTitle>프로필 설정</StTitle>
+            <StMyPageWrapper>
+                <StAvatar>
+                </StAvatar>
+                <StUserInfoWrapper>
+                    <StEmail>
+                        <label>이메일</label>
+                        <div>이메일</div>
+                    </StEmail>
+                    <StNickName>
+                        <label>닉네임</label>
+                        <input placeholder="닉네임" type='text'></input>
+                    </StNickName>
+                    <StCurrentPw>
+                        <label>현재 비밀번호</label>
+                        <input type='password'></input>
+                    </StCurrentPw>
+                    <StNewPw>
+                        <label>새로운 비밀번호</label>
+                        <input type='password'></input>
+                    </StNewPw>
+                </StUserInfoWrapper>
+            </StMyPageWrapper>
+            <StBtnsWrapper>
+                <StSubmitBtn>완료</StSubmitBtn>
+                <StDeleteBtn>삭제</StDeleteBtn>
+            </StBtnsWrapper>
+        </StContainer>
     )
 }
 
-export const Container = styled.div`
+export const StContainer = styled.div`
     display: flex;
     flex-direction: column;
     padding: 0px 300px
 `;
-
 
 export const StTitle = styled.h1`
 font-weight: 600;
@@ -80,15 +75,18 @@ export const StUserInfoWrapper = styled.div`
     background: #FFFFFF;
     border: 1px solid #838383;
     box-sizing: border-box; 
+    padding-left: 20px;
+    padding-left: 20px;
+    font-size: 24px; 
   }
 & input ::placeholder {
-    font-size: 24px !important;
 color: #838383;
 }
 `
 export const StEmail = styled.div`
 display: flex;
 flex-direction: column;
+justify-content: center;
 gap: 20px;
  & div {
 width: 100%;
@@ -98,6 +96,9 @@ background: #F2F2F2;
 font-weight: 600;
 font-size: 24px;
 color: #838383;
+display: flex;
+align-items: center; 
+padding-left: 20px;
  }
 `
 export const StNickName = styled.div`
@@ -119,10 +120,7 @@ export const StBtnsWrapper = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: flex-end;
-    gap: 30px;
-    padding: 20px;
+    gap: 20px;
+    margin-top: 20px;
 `
-
-
-
 

a25c113bc01e1e5774b329fb5ab98b532b9bb745
HuaHuaChiChi <ksksks102@naver.com>
2024-02-09 14:55:49 +0900

    [fix] 가독성 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react';
 import ResetStyles, { StAppWrapper } from './App.style.js';
 import Posting from './components/Posting/Posting.jsx';
-import MainPage from './pages/MainPage.jsx';
-import MyPage from './components/MyPage/MyPage.jsx';
-import ArticlePage from './pages/ArticlePage.jsx';
 import Modal from './components/Login/Modal.jsx';
 
 const App = () => {
   return (
     <StAppWrapper>
       <ResetStyles />
-      <ArticlePage />
+      <Posting />
       <Modal />
     </StAppWrapper>
   );

84c3c020907447fb3d53a2a9f0ad453ffa929cbd
innes-k <innes.kwak@gmail.com>
2024-02-11 19:14:54 +0900

    [feature] 로그인시 email 형식 유효성검사 구현

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import {
   StCheckBox,
   StIdPwWrapper,
@@ -18,6 +19,39 @@ import {
 import { StErrorMsg } from './styles/SignUp.style';
 
 const Login = ({ onModalHandler, onSignUpHandler }) => {
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPw, setIsValidPw] = useState(false);
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (emailRegex.test(email)) {
+      setIsValidEmail(true);
+    } else {
+      setIsValidEmail(false);
+    }
+  };
+
+  const onChangePw = (e) => {
+    setPw(e.target.value);
+  };
+
+  // useEffect(() => {
+  //   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  //   const passwordRegex =
+  //     /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  //   setIsValidEmail(emailRegex.test(email));
+  //   setIsValidPw(passwordRegex.test(pw));
+
+  //   if (isValidEmail) {
+  //   }
+  // }, [email, pw]);
+
   return (
     <>
       <StLoginWrapper>
@@ -26,18 +60,30 @@ const Login = ({ onModalHandler, onSignUpHandler }) => {
         <StLoginTitle>로그인</StLoginTitle>
         <StIdPwWrapper>
           <StInputContainer>
-            <StLoginInput placeholder='travel123@gmail.com' />
+            <StLoginInput
+              placeholder='travel123@gmail.com'
+              value={email}
+              onChange={onChangeEmail}
+            />
           </StInputContainer>
-          <StErrorMsg>올바르지 않은 이메일 형식입니다.</StErrorMsg>
+          <StErrorMsg>
+            {!isValidEmail && email.length > 0 && (
+              <div>올바르지 않은 이메일 형식입니다.</div>
+            )}
+          </StErrorMsg>
           <StInputContainer>
             <StLoginInput
               type='password'
               autoComplete='on'
               placeholder='비밀번호'
+              value={pw}
+              onChange={onChangePw}
             />
             <StPwIcon />
           </StInputContainer>
-          <StErrorMsg>비밀번호가 틀립니다.</StErrorMsg>
+          <StErrorMsg>
+            영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.
+          </StErrorMsg>
         </StIdPwWrapper>
         <StMemoWrapper>
           <StCheckBox type='checkbox' />

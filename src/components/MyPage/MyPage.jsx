c78575c235268b1d7fca6d74f29354cc77a262bb
seoyeon-1206 <ssy_1206@naver.com>
2024-02-14 11:26:27 +0900

    pull 받기 전

--- src/components/MyPage/MyPage.jsx
+++ src/components/MyPage/MyPage.jsx
@@ -21,11 +21,14 @@ import { useNavigate } from 'react-router-dom';
 import { onAuthStateChanged } from "firebase/auth";
 import db, { auth } from '../../apis/config';
 import { collection, getDocs, query, updateDoc } from 'firebase/firestore/lite';
+import { doc } from '@firebase/firestore';
 
 const MyPage = () => {
   const [nickname, setNickname] = useState("")
   const [email, setEmail] = useState("")
   const [image, setImage] = useState("")
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -38,6 +41,7 @@ const MyPage = () => {
         const querySnapshot = await getDocs(q);
         querySnapshot.docs.forEach((item) => {
           const { nickname, email, image } = item.data();
+          console.log(item.data)
           setNickname(nickname)
           setEmail(email)
           setImage(image)
@@ -49,42 +53,55 @@ const MyPage = () => {
     fetchData();
   }, [])
 
-  //Firestore에서 사용자 데이터 업데이트하는 함수
-  const updateUserInFirestore = async (updatedData) => {
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log("user?????", user);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  // 닉네임 업데이트
+  const updateNickname = async () => {
     try {
-      const q = query(collection(db, 'users'));
-      const querySnapshot = await getDocs(q);
-      querySnapshot.docs.forEach(async (item) => {
-        const docId = item.id;
-        const userRef = item(db, 'users', docId);
-        await updateDoc(userRef, updatedData);
-        dispatch(updateUserData(updatedData));
-      });
+      const user = auth.currentUser;
+      if (user) {
+        const uid = user.uid;
+        const usersRef = doc(db, 'users', uid);
+        await updateDoc(usersRef, { nickname: nickname });
+        dispatch(updateUserData({ nickname: nickname }));
+        alert('닉네임이 업데이트되었습니다.');
+      } else {
+        console.error('No user is currently signed in.');
+      }
     } catch (error) {
-      console.log("firestore 업데이트 실패:", error);
+      console.error('Error updating nickname:', error);
     }
-  }
+  };
+
 
   const handleUpdate = () => {
-    const updatedData = { email, nickname, image }
-    updateUserInFirestore(updatedData)
-    console.log(updatedData)
+    // const updatedData = { email, nickname, image }
+    // updateUserInFirestore(updatedData)
+    // console.log(updatedData)
     // let updateUser = { ...user };
     // // 닉네임을 변경한 경우
     // if (nickname !== user.nickname) {
-    //   updateUser = { ...updateUser, nickname }
+    //   // 닉네임 저장 로직
     // };
-    // // 새로운 비밀번호를 입력하고, 확인란과 일치하는 경우
-    // if (newPassword && newPassword === confirmPassword) {
-    //   updateUser = { ...updateUser, nono: newPassword };
-    // }
-    // // 비밀번호 유효성 검사
-    // if (newPassword !== confirmPassword) {
-    //   alert('비밀번호가 일치하지 않습니다. 다시 확인해주세요.');
-    //   setConfirmPassword('')
-    //   return;
-    // }
-    // // 이미지를 변경한 경우 
+    updateNickname();
+
+
+    // 새로운 비밀번호를 입력하고, 확인란과 일치하는 경우
+    if (newPassword && newPassword === confirmPassword) {
+
+    }
+    // 비밀번호 유효성 검사
+    if (newPassword !== confirmPassword) {
+      alert('비밀번호가 일치하지 않습니다. 다시 확인해주세요.');
+      setConfirmPassword('')
+      return;
+    }
+    // 이미지를 변경한 경우 
     // if (image !== user.image) {
     //   updateUser = { ...updateUser, image };
     // }
@@ -94,7 +111,6 @@ const MyPage = () => {
   }
 
   const handleCancle = () => {
-    // 이전 페이지로 돌아가기?
     navigate(-1);
   }
 
@@ -117,11 +133,11 @@ const MyPage = () => {
               </StNickName>
               <StCurrentPw>
                 <label>새로운 비밀번호</label>
-                <input placeholder="비밀번호" type='password'></input>
+                <input placeholder="비밀번호" type='password' value={newPassword} onChange={(e) => setNewPassword(e.target.value)}></input>
               </StCurrentPw>
               <StNewPw>
                 <label>비밀번호 확인</label>
-                <input type='password'></input>
+                <input type='password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} ></input>
               </StNewPw>
             </StUserInfoDeatilWrapper>
           </StUserInfoWrapper>
